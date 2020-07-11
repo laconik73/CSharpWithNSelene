f69b40a884ffd009ad468b12aa37a2bc8be89bb5
Sabina Dovlati <47467090+laconik73@users.noreply.github.com>
2020-07-10 22:43:06 -0400

    Add files via upload
    added app.config and base pages

---
+++ ProjectPageBase.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+namespace Ocaramba.Tests.PageObjects
+{
+    using Ocaramba;
+    using OpenQA.Selenium;
+
+    public partial class ProjectPageBase
+    {
+        public ProjectPageBase(DriverContext driverContext)
+        {
+            this.DriverContext = driverContext;
+            this.Driver = driverContext.Driver;
+            //Driver.Manage().Window.Size = new System.Drawing.Size(1680, 1050);
+            Driver.Manage().Window.Maximize();
+        }
+
+        protected IWebDriver Driver { get; set; }
+
+        public ProjectPageBase()
+        {
+
+        }
+        protected DriverContext DriverContext { get; set; }
+    }
+}
